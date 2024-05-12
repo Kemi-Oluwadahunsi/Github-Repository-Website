757073532dc9e5d85848cac854ae4f9cbb409e5c
Oluwakemi Oluwadahunsi <kemilat50@gmail.com>
2024-05-12 17:40:56 +0800

    Update ErrorBoundary.jsx

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -16,8 +16,8 @@ function ErrorBoundary({ children }) {
   }, []);
 
   if (hasError) {
-    //  render any custom fallback UI
-    return <div className="h-[70vh] flex justify-center items-center"><h1 className="text-center text-[3rem] font-bold">Something went wrong. Please try again later.</h1></div>;
+    //  custom fallback UI
+    return <div className="h-[70vh] flex justify-center items-center"><h1 className="text-center text-[3rem] font-bold">Something went wrong. View console for details.</h1></div>;
   }
 
   return <>{children}</>;
